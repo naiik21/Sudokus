7eb6e4140def402b2eecced0cd46a457f7b6a8ab
Nicolas Ratia Parejo <115487786+naiik21@users.noreply.github.com>
2023-11-30 14:01:37 +0100

    skere

--- sudokus.js
+++ sudokus.js
@@ -8,20 +8,15 @@ function iniciarPartida() {
 
 function crearTaulell() {
     
-    for(let a=1; a<10;a++){
-        let nums=[1,2,3,4,5,6,7,8,9];
+    for (let a = 1; a < 10; a++) {
         let taula = "<table>";
-        let bloc=[];
+        let nums=[1,2,3,4,5,6,7,8,9];
         for (let n = 0; n < 3; n++) {
             taula += "<tr>";
             for (let i = 0; i < 3; i++) {
-                let numIndex
-                let num;
-                numIndex = Math.floor(Math.random() * nums.length );
-                num=nums.at(numIndex);
-                revisaFila(n, i, a);
-                taula += `<td id="${n}-${i}-${a}" data-fila='${n}' data-columna='${i}' data-bloc='${a}' data-num='${num}'><input type="number" min="1" max="9">${num}</td>`; 
-                nums.splice(numIndex,1)[0];
+                let numIndex = Math.floor(Math.random() * nums.length);
+                let num = nums.splice(numIndex, 1)[0];
+                taula += `<td id="${n}-${i}-${a}" data-fila='${n}' data-columna='${i}' data-bloc='${a}' data-num='${num}'><input type="number" min="1" max="9">${num}</td>`;
             }
             taula += "</tr>";
         }
@@ -29,28 +24,41 @@ function crearTaulell() {
         document.getElementById(`${a}`).innerHTML = taula;
     }
 
+    revisarColumnas();
+    revisarBloques();
+    
+
 }
 
-function revisaFila(fila, columna, bloc){
+function revisaFila(fila, columna, bloc, nums){
     let td1 = document.getElementById(`${fila}-${columna}-${bloc}`);
     for(let n=1; n<=3; n++){
         for(let i=0; i<3;i++){
             let tdAux=document.getElementById(`${fila}-${i}-${n}`);
-        console.log(`${fila}-${i}-${n}`);
-        try{
-            console.log(td1.dataset.num + "-"+ tdAux.dataset.num);
-            if(td1.dataset.columna == tdAux.dataset.columna){
+            console.log(`${fila}-${i}-${n}`);
+            console.log(td1.dataset.num + '-' + tdAux.dataset.num);
+            try{
+                if((td1.dataset.columna == tdAux.dataset.columna)  && (td1.dataset.fila == tdAux.dataset.fila) && (td1.dataset.bloc == tdAux.dataset.bloc)){
                 console.log("salto");
                 continue;
             }else{
-                if(td1.dataset.num == tdAux.dataset.num){
-                    console.log("cuidado");
-                    return false;
+                while(td1.dataset.num == tdAux.dataset.num){
+                    console.log(td1.dataset.num +'-'+ tdAux.dataset.num);
+                    let numIndex
+                    let num;
+                    numIndex = Math.floor(Math.random() * nums.length );
+                    num=nums.at(numIndex);
+                    console.log(num);
+                    tdAux.setAttribute("data-num", num);
+                    //document.getElementById(`${fila}-${i}-${n}`).innerHTML = `<td id="${fila}-${i}-${n}" data-fila='${fila}' data-columna='${i}' data-bloc='${n}' data-num='${num}'><input type="number" min="1" max="9">${num}</td>`;
+                    tdAux=document.getElementById(`${fila}-${columna}-${bloc}`);
+                    console.log(tdAux.dataset.num);
                 }
             }
 
         }catch (e){
-
+            console.log("error");
+            console.log(e);
         }
         }
             
@@ -61,22 +69,46 @@ function revisaFila(fila, columna, bloc){
 
 
 /*
-let numIndex
-                do{
-                    numIndex = Math.floor(Math.random() * nums.length );
-                    //let num=nums.splice(numIndex,1)[0];
-                    let num=nums.at(numIndex);
-                }while(revisaFila(n, i));
-                taula += `<td id="${n}-${i}" data-fila='${n}' data-columna='${i}' data-bloc='${a}' data-num='${num}'><input type="number" min="1" max="9">${num}</td>`; 
-                nums.splice(numIndex,1)[0];
-                */
+function revisarColumnas() {
+    for (let n = 0; n < 3; n++) {
+        for (let i = 0; i < 3; i++) {
+            revisaFila(n, i);
+        }
+    }
+}
 
+function revisarBloques() {
+    for (let a = 1; a < 10; a++) {
+        let nums = [];
+        for (let i = 1; i <= 9; i++) {
+            nums.push(i);
+        }
 
+        for (let n = 0; n < 3; n++) {
+            for (let i = 0; i < 3; i++) {
+                revisaFila(n, i, a, nums);
+            }
+        }
+    }
+}
 
-/*
-let numIndex = Math.floor(Math.random() * nums.length );
-                let num=nums.splice(numIndex,1)[0];
-
-                taula += `<td id="${n}-${i}" data-fila='${n}' data-columna='${i}' data-bloc='${a}' data-num='${num}'><input type="number" min="1" max="9">${num}</td>`; 
-                revisaFila(n, i);
+function revisaFila(fila, columna, bloc, nums) {
+    let td1 = document.getElementById(`${fila}-${columna}-${bloc}`);
+    
+    for (let n = 1; n <= 3; n++) {
+        for (let i = 0; i < 3; i++) {
+            let tdAux = document.getElementById(`${fila}-${i}-${n}`);
+            if (td1.dataset.columna == tdAux.dataset.columna && td1.dataset.fila == tdAux.dataset.fila && td1.dataset.bloc == tdAux.dataset.bloc) {
+                continue;
+            } else {
+                while (td1.dataset.num == tdAux.dataset.num) {
+                    let numIndex = Math.floor(Math.random() * nums.length);
+                    let num = nums.splice(numIndex, 1)[0];
+                    tdAux.setAttribute("data-num", num);
+                    tdAux.innerHTML = `<input type="number" min="1" max="9">${num}`;
+                }
+            }
+        }
+    }
+}
 */
